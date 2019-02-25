33ce81b3388b952518f244dc10fa09faab62dec5
hiramasa1175 <hiramasa1175@gmail.com>
2019-02-26 01:31:30 +0900

    ok

--- app/src/main/java/com/example/mp4parser/MainActivity.java
+++ app/src/main/java/com/example/mp4parser/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mp4parser;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parser.append();
+        Parser.test();
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath(Environment.getExternalStorageDirectory().toString() + "/Movies/output2.mp4");
+        videoView.start();
+        videoView.setMediaController(new MyMediaController(this));
     }
 }
--- app/src/main/java/com/example/mp4parser/Parser.java
+++ app/src/main/java/com/example/mp4parser/Parser.java
@@ -11,22 +11,27 @@ import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Parser {
-    private boolean append() {
+    public static boolean append() {
         try {
             // 複数の動画を読み込み
-            String f1 = Environment.getExternalStorageDirectory() + "/sample1.mp4";
-            String f2 = Environment.getExternalStorageDirectory() + "/sample2.mp4";
+            String f1 = Environment.getExternalStorageDirectory() + "/Movies/sample3.mp4";
+            String f2 = Environment.getExternalStorageDirectory() + "/Movies/sample4.mp4";
+
             Movie[] inMovies = new Movie[]{
                     MovieCreator.build(f1),
-                    MovieCreator.build(f2)};
+                    MovieCreator.build(f2)
+            };
 
             // 1つのファイルに結合
-            List<Track> videoTracks = new LinkedList<Track>();
-            List<Track> audioTracks = new LinkedList<Track>();
+            List<Track> videoTracks = new LinkedList<>();
+            List<Track> audioTracks = new LinkedList<>();
             for (Movie m : inMovies) {
                 for (Track t : m.getTracks()) {
                     if (t.getHandler().equals("soun")) {
@@ -47,11 +52,58 @@ public class Parser {
 
             // 出力
             Container out = new DefaultMp4Builder().build(result);
-            String outputFilePath = Environment.getExternalStorageDirectory() + "/output_append.mp4";
+            String outputFilePath = Environment.getExternalStorageDirectory() + "/Movies/output.mp4";
             FileOutputStream fos = new FileOutputStream(new File(outputFilePath));
             out.writeContainer(fos.getChannel());
             fos.close();
         } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean test() {
+        // 複数の動画を読み込み
+        String f1 = Environment.getExternalStorageDirectory() + "/Movies/output.mp4";
+        String f2 = Environment.getExternalStorageDirectory() + "/Music/sound.m4a";
+
+        Movie audioTrack = null;
+        Movie outputVideo = null;
+        try {
+            outputVideo = MovieCreator.build(f1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        try {
+            audioTrack = MovieCreator.build(f2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        List<Track> nuTracks = new ArrayList<>();
+        for (Track track : outputVideo.getTracks()) {
+            if (track.getHandler().equals("vide")) {
+                nuTracks.add(track);
+            }
+        }
+        for (Track track : audioTrack.getTracks()) {
+            if (track.getHandler().equals("soun")) {
+                nuTracks.add(track);
+            }
+        }
+
+        outputVideo.setTracks(nuTracks);
+        Container out = new DefaultMp4Builder().build(outputVideo);
+        try {
+            String outputFilePath = Environment.getExternalStorageDirectory() + "/Movies/output2.mp4";
+            FileOutputStream fos = new FileOutputStream(new File(outputFilePath));
+            FileChannel fc = fos.getChannel();
+            out.writeContainer(fc);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
             return false;
         }
 
